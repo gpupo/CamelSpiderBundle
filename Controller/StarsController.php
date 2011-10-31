186659f104c31882d181f138ef7c6779fc8c4c2b
Gilmar Pupo <gilmar@gilmar.eti.br>
2011-10-31 11:19:01 -0200

    add starsController

--- Controller/StarsController.php
+++ Controller/StarsController.php
@@ -2,7 +2,29 @@
 
 namespace Gpupo\CamelSpiderBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response;
+
 class StarsController
 {
+public function submitAction(Request $request){
+   /* $request = $this->get('request');
+    $name=$request->request->get('formName');
+   
+   if($name!=""){//if the user has written his name
+      $greeting='Hello '.$name.'. How are you today?';
+      $return=array("responseCode"=>200,  "greeting"=>$greeting);
+   }
+   else{
+      $return=array("responseCode"=>400, "You have to write your name!");
+   }
+    */
+    
+    $form = $request->request->get('vote');
+      $return=array("responseCode"=>200, 'news_id'=> $form['news_id'],  "average"=>5);
+   $return=json_encode($return);//jscon encode the array
+      return new Response($return,200);
+}
 
 }
