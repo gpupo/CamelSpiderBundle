c7da1211197c7918a66b568441a2704e54b3ff8b
Rafael Goulart <rafaelgou@gmail.com>
2011-10-31 22:47:00 -0200

    Adicionada votação (estrelas)

--- Controller/StarsController.php
+++ Controller/StarsController.php
@@ -4,27 +4,93 @@ namespace Gpupo\CamelSpiderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller,
     Symfony\Component\HttpFoundation\Request,
-    Symfony\Component\HttpFoundation\Response;
+    Symfony\Component\HttpFoundation\Response,
+    Gpupo\CamelSpiderBundle\Entity\News,
+    Gpupo\CamelSpiderBundle\Entity\NewsVote
+    ;
 
-class StarsController
+class StarsController extends Controller
 {
-public function submitAction(Request $request){
-   /* $request = $this->get('request');
-    $name=$request->request->get('formName');
-   
-   if($name!=""){//if the user has written his name
-      $greeting='Hello '.$name.'. How are you today?';
-      $return=array("responseCode"=>200,  "greeting"=>$greeting);
-   }
-   else{
-      $return=array("responseCode"=>400, "You have to write your name!");
-   }
-    */
-    
-    $form = $request->request->get('vote');
-      $return=array("responseCode"=>200, 'news_id'=> $form['news_id'],  "average"=>5);
-   $return=json_encode($return);//jscon encode the array
-      return new Response($return,200);
-}
+    public function submitAction(Request $request)
+    {
+        $form    = $request->request->get('vote');
+        $news_id = $form['news_id'];
+        $rate    = $form['rate'];
+
+        $manager = $this->getDoctrine()->getEntityManager();
+
+        $news = $manager->getRepository('GpupoCamelSpiderBundle:News')->find($form['news_id']);
+
+        if (!$news) {
+            // Response com "erro"
+            $return = array("responseCode"=>400, 'News not found');
+            return new Response(json_encode($return),400);
+
+        } else {
+            // Salvar voto, tem que ser entre 1 e 5 o valor
+            try {
+                $vote = new NewsVote();
+                $vote->setNews($news);
+                $vote->getValue((int)$form['rate']);
+                $manager->persist($vote);
+                $manager->flush();
+                $responseCode = 200;
+            } catch (Exception $e) {
+                $return = array("responseCode"=>400, 'Error adding vote');
+                return new Response(json_encode($return),400);
+            }
+
+            $query = $manager->createQuery('SELECT v, AVG(v.value) as average FROM NewsVote WHERE v.news_id = ' . $form['news_id'] . ' GROUP BY v.news_id');
+            $result = $query->getResult();
+            $result->
+
+            $return = array("responseCode"=>$responseCode, 'news_id'=> $form['news_id'],  "average"=>$average);
+            return new Response(json_encode($return),200);
+        }
+
+    }
+
+    public function submitTestAction($news_id, $rate)
+    {
+        $manager = $this->getDoctrine()->getEntityManager();
+
+        $news = $manager->getRepository('GpupoCamelSpiderBundle:News')->find($news_id);
+
+        if (!$news) {
+            // Response com "erro"
+            $return = array("responseCode"=>400, 'News not found');
+            return new Response(json_encode($return),400);
+
+        } else {
+            // Salvar voto, tem que ser entre 1 e 5 o valor
+            try {
+                $vote = new NewsVote();
+                $vote->setNews($news);
+                $vote->setValue($rate);
+                $manager->persist($vote);
+                $manager->flush();
+                $responseCode = 200;
+            } catch (Exception $e) {
+                $return = array("responseCode"=>400, 'Error adding vote');
+                return new Response(json_encode($return),400);
+            }
+
+            $query = $manager->createQueryBuilder()
+                    ->addSelect('AVG(v.value) as average')
+                    ->from('GpupoCamelSpiderBundle:NewsVote', 'v')
+                    ->join('v.news', 'n')
+                    ->where('n.id = ' . $news_id)
+                    ->groupBy('n.id');
+            $result = $query->getQuery()->execute();
+
+            $return = array(
+                'responseCode' => $responseCode,
+                'news_id'      => $news_id,
+                'average'      => round($result[0]['average'], 0, PHP_ROUND_HALF_DOWN)
+                );
+            return new Response(json_encode($return),200);
+        }
+
+    }
 
 }
--- Entity/NewsVote.php
+++ Entity/NewsVote.php
@@ -4,6 +4,7 @@ namespace Gpupo\CamelSpiderBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Funpar\AdminBundle\Entity\User as User;
 
 /**
  * Gpupo\CamelSpiderBundle\Entity\NewsVote
@@ -31,9 +32,70 @@ class NewsVote
     private $value;
 
     /**
-     * @ORM\OneToOne(targetEntity="News")
+     * @ORM\ManyToOne(targetEntity="News")
      * @ORM\JoinColumn(name="news_id", referencedColumnName="id")
      */
     private $news;
 
+//    /**
+//     * @ORM\ManyToOne(targetEntity="\Funpar\AdminBundle\Entity\User")
+//     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+//     */
+//    private $user;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param integer $value
+     */
+    public function setValue($value)
+    {
+       $value = intval($value);
+
+        if ($value < 1 || $value > 5 ) {
+            throw new InvalidArgumentException('A vote must be between 1 and 5');
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * Get value
+     *
+     * @return integer
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set news
+     *
+     * @param Gpupo\CamelSpiderBundle\Entity\News $news
+     */
+    public function setNews(\Gpupo\CamelSpiderBundle\Entity\News $news)
+    {
+        $this->news = $news;
+    }
+
+    /**
+     * Get news
+     *
+     * @return Gpupo\CamelSpiderBundle\Entity\News
+     */
+    public function getNews()
+    {
+        return $this->news;
+    }
 }
