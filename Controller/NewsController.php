8e93cd87e81bdeae12cb5252ba6b179bf06d7f18
Rafael Goulart <rafaelgou@gmail.com>
2011-11-07 14:35:09 -0200

    Customized News list to remove 'view' action

--- Controller/NewsController.php
+++ Controller/NewsController.php
@@ -14,4 +14,28 @@ class NewsController extends GeneratorController
         $this->loadGenerator($generator);
     }
 
+    public function indexAction()
+    {
+        // Configuring the Generator Controller
+        $this->configure();
+
+        // Defining filters
+        $this->configureFilter();
+
+        // Defining actual page
+        $this->setPage($this->getRequest()->get('page', $this->getPage()));
+
+        $pager = $this->getPager();
+
+        $deleteForm = $this->createDeleteForm('fake');
+
+        return $this->render('GpupoCamelSpiderBundle:News:listGrid.html.twig', array(
+            'pager'      => $pager,
+            'delete_form' => $deleteForm->createView(),
+            'generator'   => $this->generator,
+        ));
+
+    }
+
+
 }
