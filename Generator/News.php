fc101ca74ac7b1ed0f1a7a08eff79c911f395615
Rafael Goulart <rafaelgou@gmail.com>
2011-12-05 09:41:04 -0200

    Retornando  max_per_page de News para 30

--- Generator/News.php
+++ Generator/News.php
@@ -57,7 +57,7 @@ class News extends Generator
             'stackedTemplate' => '<h3>{{ record.name  }}</h3>' .
                                  '<p class="details_fixed">URI: <strong>{{ record.uri }}</strong></p>',
             'sort'            => array('createdAt' =>  'DESC', 'id' => 'DESC'),
-            'max_per_page'    => 3,
+            'max_per_page'    => 30,
             'object_actions'  => array(),
             'batch_actions'   => array(),
         );
